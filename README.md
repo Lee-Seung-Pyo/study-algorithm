d5a6d46df5310e2d6ee3028e980287db3c261163
splee <zlmmxe@naver.com>
2020-07-01 17:08:00 +0900

    Add BOJ2193

--- README.md
+++ README.md
@@ -64,6 +64,7 @@ for preparing 'coding test'
 [BOJ2293 동전 1](/src/BOJ2293.md)  
 [BOJ11051 이항 계수 2](/src/BOJ11051.md)  
 [BOJ9095 1, 2, 3 더하기](/src/BOJ9095.md)  
+[BOJ2193 이친수](/src/BOJ2193.md)  
 
 ### 그리디
 [BOJ10610 30](/src/BOJ10610.md)  
--- src/SolvingProblem/SolvingProblem.cpp
+++ src/SolvingProblem/SolvingProblem.cpp
@@ -2,46 +2,18 @@
 #define endl '\n'
 using namespace std;
 
-pair<int, int> f[100001];
-int mtod[13] = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 };
+pair<long long, long long> dp[91]; //(n자리 이친수가 0으로 끝나는 개수, 1로 끝나는 개수)
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int st_m, st_d, en_m, en_d;
-		cin >> st_m >> st_d >> en_m >> en_d;
-		f[i] = { mtod[st_m - 1] + st_d, mtod[en_m - 1] + en_d };
+	dp[1] = { 0,1 };
+	dp[2] = { 1,0 };
+	for (int i = 3; i <= n; i++) {
+		dp[i] = { dp[i - 1].first + dp[i - 1].second, dp[i - 1].first };
 	}
-	sort(f, f + n);
-	int cnt = 0, cur = 60, mx_en = 0, i = 0;
-	bool flag = false;
-	while (i < n) {
-		int st = f[i].first, en = f[i].second;
-		if (st > cur) { // cur 날짜 최신화
-			if (!flag) { // cur 날짜에 놓을 꽃이 하나도 없는 경우
-				cout << 0;
-				return 0;
-			}
-			cur = mx_en;
-			cnt++;
-			flag = false;
-			if (cur > 334) {
-				cout << cnt;
-				return 0;
-			}
-			continue;
-		}
-		i++;
-		if (en <= cur) continue;
-		flag = true;
-		mx_en = max(mx_en, en);
-	}
-	// i가 n이 되서 while문을 나오는 경우 - 아직 마지막 꽃을 정하지 못함
-	if (mx_en <= 334) cout << 0;
-	else cout << ++cnt;
-
+	cout << dp[n].first + dp[n].second;
 	return 0;
 }
