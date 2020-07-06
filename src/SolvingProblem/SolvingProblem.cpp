a90aa907dc023968b99b9348f3ba920d6a8156f7
splee <zlmmxe@naver.com>
2020-07-06 18:18:57 +0900

    Add BOJ2167

--- src/SolvingProblem/SolvingProblem.cpp
+++ src/SolvingProblem/SolvingProblem.cpp
@@ -2,26 +2,27 @@
 #define endl '\n'
 using namespace std;
 
-int num[50];
-char pm[50];
+int cumul[301][301];
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	string s;
-	cin >> s;
-	stringstream ss(s);
-	int n = 0;
-	do ss >> num[n++];
-	while (ss >> pm[n]);
-	int ans = num[0];
-	bool plus = true;
-	for (int i = 1; i < n; i++) {
-		if (pm[i] == '-') plus = false;
-		if (plus) ans += num[i];
-		else ans -= num[i];
+	int n, m;
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			int input;
+			cin >> input;
+			cumul[i][j] = input + cumul[i - 1][j] + cumul[i][j - 1] - cumul[i - 1][j - 1];
+		}
 	}
-	cout << ans;
+	int k;
+	cin >> k;
+	while (k--) {
+		int i, j, x, y;
+		cin >> i >> j >> x >> y;
+		cout << cumul[x][y] - cumul[x][j - 1] - cumul[i - 1][y] + cumul[i - 1][j - 1] << endl;
+	}
 
 	return 0;
 }
