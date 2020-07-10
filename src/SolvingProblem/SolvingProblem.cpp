ddf69b0ad31abe1ac2f362f71e1bfc57ca369ae8
splee <zlmmxe@naver.com>
2020-07-10 16:28:19 +0900

    Add BOJ11403

--- src/SolvingProblem/SolvingProblem.cpp
+++ src/SolvingProblem/SolvingProblem.cpp
@@ -2,43 +2,27 @@
 #define endl '\n'
 using namespace std;
 
-vector<int> adj[20001];
-vector<bool> vis(20001, false);
+int adj[100][100];
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int n, m;
-	cin >> n >> m;
-	while (m--) {
-		int a, b;
-		cin >> a >> b;
-		adj[a].push_back(b);
-		adj[b].push_back(a);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) cin >> adj[i][j];
 	}
-	queue<pair<int, int>> q; //node,dist
-	q.push({ 1,0 });
-	vis[1] = true;
-	int dist = 0, cnt = 1, ni = 1;
-	while (!q.empty()) {
-		int cur_n, cur_d;
-		tie(cur_n, cur_d) = q.front(); q.pop();
-		if (dist == cur_d) {
-			ni = min(ni, cur_n);
-			cnt++;
-		}
-		else {
-			dist = cur_d;
-			ni = cur_n;
-			cnt = 1;
-		}
-		for (auto e : adj[cur_n]) {
-			if (vis[e]) continue;
-			q.push({ e, cur_d + 1 });
-			vis[e] = true;
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (!adj[i][j] && adj[i][k] && adj[k][j]) adj[i][j] = 1;
+			}
 		}
 	}
-	cout << ni << ' ' << dist << ' ' << cnt;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) cout << adj[i][j] << ' ';
+		cout << endl;
+	}
 
 	return 0;
 }
