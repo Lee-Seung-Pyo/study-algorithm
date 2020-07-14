71f8b36af78cb112b61a2ad6f6d1d8fc37804b5c
splee <zlmmxe@naver.com>
2020-07-14 13:50:50 +0900

    Add BOJ14888

--- src/SolvingProblem/SolvingProblem.cpp
+++ src/SolvingProblem/SolvingProblem.cpp
@@ -2,50 +2,44 @@
 #define endl '\n'
 using namespace std;
 
-long long fact[21] = { 1 };
-int arr[20];
-bool isused[21];
-int n, p;
-
-void p1(long long k, int depth) {
-	if (depth > n) return;
-	int order = k / fact[n - depth];
-	for (int i = 1; i <= n; i++) {
-		if (isused[i]) continue;
-		if (!order--) {
-			arr[depth] = i;
-			isused[i] = true;
-			break;
-		}
-	}
-	p1(k % fact[n - depth], depth + 1);
-}
+int num[11];
+vector<int> oprtr; //0(+) 1(-) 2(*) 3(/)
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	cin >> n >> p;
-	for (int i = 1; i <= n; i++) fact[i] = fact[i - 1] * i;
-	if (p == 1) {
-		long long k;
-		cin >> k;
-		p1(k - 1, 1);
-		for (int i = 1; i <= n; i++) cout << arr[i] << ' ';
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> num[i];
+	for (int i = 0; i < 4; i++) {
+		int cnt;
+		cin >> cnt;
+		while (cnt--) oprtr.push_back(i);
 	}
-	else {
-		long long k = 0;
-		for (int i = 1; i <= n; i++) {
-			int cur;
-			cin >> cur;
-			isused[cur] = true;
-			int cnt = 0;
-			for (int j = 1; j < cur; j++) {
-				if (!isused[j]) cnt++;
+	int mx = INT_MIN, mn = INT_MAX;
+	do {
+		int tmp = num[0];
+		for (int i = 1; i < n; i++) {
+			switch (oprtr[i - 1]) {
+			case 0:
+				tmp += num[i];
+				break;
+			case 1:
+				tmp -= num[i];
+				break;
+			case 2:
+				tmp *= num[i];
+				break;
+			case 3:
+				tmp /= num[i];
+				break;
 			}
-			k += cnt * fact[n - i];
 		}
-		cout << ++k;
-	}
+		mx = max(mx, tmp);
+		mn = min(mn, tmp);
+	} while (next_permutation(oprtr.begin(), oprtr.end()));
+	cout << mx << endl;
+	cout << mn << endl;
 
 	return 0;
 }
