c1c4322ad2ffb648e72f897015dcb642492ca1da
splee <zlmmxe@naver.com>
2020-07-14 14:39:53 +0900

    Add BOJ16987

--- src/SolvingProblem/SolvingProblem.cpp
+++ src/SolvingProblem/SolvingProblem.cpp
@@ -2,44 +2,44 @@
 #define endl '\n'
 using namespace std;
 
-int num[11];
-vector<int> oprtr; //0(+) 1(-) 2(*) 3(/)
+int s[8];
+int w[8];
+int n;
+int mx = 0;
+
+void recur(int cur) {
+	if (cur == n) {
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			if (s[i] <= 0) cnt++;
+		}
+		mx = max(mx, cnt);
+		return;
+	}
+	if (s[cur] <= 0) {
+		recur(cur + 1);
+		return;
+	}
+	bool flag = false;
+	for (int i = 0; i < n; i++) {
+		if (i == cur || s[i] <= 0) continue;
+		flag = true;
+		s[cur] -= w[i];
+		s[i] -= w[cur];
+		recur(cur + 1);
+		s[cur] += w[i];
+		s[i] += w[cur];
+	}
+	if (!flag) recur(cur + 1);
+}
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++) cin >> num[i];
-	for (int i = 0; i < 4; i++) {
-		int cnt;
-		cin >> cnt;
-		while (cnt--) oprtr.push_back(i);
-	}
-	int mx = INT_MIN, mn = INT_MAX;
-	do {
-		int tmp = num[0];
-		for (int i = 1; i < n; i++) {
-			switch (oprtr[i - 1]) {
-			case 0:
-				tmp += num[i];
-				break;
-			case 1:
-				tmp -= num[i];
-				break;
-			case 2:
-				tmp *= num[i];
-				break;
-			case 3:
-				tmp /= num[i];
-				break;
-			}
-		}
-		mx = max(mx, tmp);
-		mn = min(mn, tmp);
-	} while (next_permutation(oprtr.begin(), oprtr.end()));
-	cout << mx << endl;
-	cout << mn << endl;
+	for (int i = 0; i < n; i++) cin >> s[i] >> w[i];
+	recur(0);
+	cout << mx;
 
 	return 0;
 }
