309219d4b0154ba0e4dac033b5abf577f4234ed7
splee <zlmmxe@naver.com>
2020-07-15 17:47:45 +0900

    Add BOJ11048

--- src/SolvingProblem/SolvingProblem.cpp
+++ src/SolvingProblem/SolvingProblem.cpp
@@ -2,24 +2,21 @@
 #define endl '\n'
 using namespace std;
 
-int dp[10001];
-const int mx = 100'000;
+int dp[1001][1001];
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int n, k;
-	cin >> n >> k;
-	fill(dp + 1, dp + k + 1, mx);
-	while (n--) {
-		int p;
-		cin >> p;
-		for (int i = p; i <= k; i++) {
-			dp[i] = min(dp[i], dp[i - p] + 1);
+	int n, m;
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			int p;
+			cin >> p;
+			dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + p;
 		}
 	}
-	if (dp[k] == mx) cout << -1;
-	else cout << dp[k];
+	cout << dp[n][m];
 
 	return 0;
 }
