19326d875f082e653f9915606600f8a3cfe9ec3c
splee <zlmmxe@naver.com>
2020-07-16 12:44:43 +0900

    Add BOJ11722

--- src/SolvingProblem/SolvingProblem.cpp
+++ src/SolvingProblem/SolvingProblem.cpp
@@ -2,18 +2,24 @@
 #define endl '\n'
 using namespace std;
 
-int dp[31] = { 1 };
+int a[1001];
+int dp[1001];
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	int n;
 	cin >> n;
-	for (int i = 2; i <= n; i += 2) {
-		dp[i] = dp[i - 2] * 3;
-		for (int j = i - 4; j >= 0; j -= 2) dp[i] += dp[j] * 2;
+	for (int i = 1; i <= n; i++) cin >> a[i];
+	fill(dp + 1, dp + n + 1, 1);
+	for (int i = 2; i <= n; i++) {
+		for (int j = 1; j < i; j++) {
+			if (a[j] > a[i]) dp[i] = max(dp[i], dp[j] + 1);
+		}
 	}
-	cout << dp[n];
+	int ans = 0;
+	for (int i = 1; i <= n; i++) ans = max(ans, dp[i]);
+	cout << ans;
 
 	return 0;
 }
