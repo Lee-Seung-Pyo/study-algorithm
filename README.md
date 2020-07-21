acd24a7e086806b1d05e7fd6bcd55fc4998146f9
splee <zlmmxe@naver.com>
2020-07-21 15:48:07 +0900

    Add BOJ15686

--- README.md
+++ README.md
@@ -62,7 +62,8 @@
 [BOJ14891 톱니바퀴](/src/BOJ14891.md)  
 [BOJ15683 감시](/src/BOJ15683.md)  
 [BOJ18808 스티커 붙이기](/src/BOJ18808.md)  
-[BOJ12100 2048 (Easy)](/src/BOJ12100.md)   
+[BOJ12100 2048 (Easy)](/src/BOJ12100.md)  
+[BOJ15686 치킨 배달](/src/BOJ15686.md)  
 
 ### 정렬  
 [BOJ11728 배열 합치기](/src/BOJ11728.md)  
--- src/SolvingProblem/SolvingProblem.cpp
+++ src/SolvingProblem/SolvingProblem.cpp
@@ -2,57 +2,42 @@
 #define endl '\n'
 using namespace std;
 
-int board[20][20];
-int n, ans;
-
-void push(int before[20][20], int after[20][20]) {
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			after[i][j] = 0;
-	for (int i = 0; i < n; i++) {
-		int cur = 0;
-		for (int j = 0; j < n; j++) {
-			if (!before[i][j]) continue;
-			if (!after[i][cur]) after[i][cur] = before[i][j];
-			else if (after[i][cur] == before[i][j]) after[i][cur++] *= 2;
-			else after[i][++cur] = before[i][j];
-		}
-	}
-}
-
-void rotate(int arr[20][20]) {
-	int tmp[20][20] = {};
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			tmp[i][j] = arr[i][j];
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			arr[i][j] = tmp[n - j - 1][i];
-}
-
-void recur(int before[20][20], int k) {
-	if (k == 5) {
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++)
-				ans = max(ans, before[i][j]);
-		return;
-	}
-	int after[20][20] = {};
-	for (int i = 0; i < 4; i++) {
-		push(before, after);
-		recur(after, k + 1);
-		rotate(before);
-	}
-}
+int n, m;
+vector<pair<int, int>> house;
+vector<pair<int, int>> chicken;
+vector<bool> is_closed;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			cin >> board[i][j];
-	recur(board, 0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			int x;
+			cin >> x;
+			if (x == 0) continue;
+			if (x == 1) house.push_back({ i,j });
+			else { // x == 2
+				chicken.push_back({ i,j });
+				is_closed.push_back(--m < 0);
+			}
+		}
+	}
+	int ans = INT_MAX;
+	do {
+		int sum = 0;
+		for (auto h : house) {
+			int x = h.first, y = h.second;
+			int unit_dist = INT_MAX;
+			for (int i = 0; i < chicken.size(); i++) {
+				if (is_closed[i]) continue;
+				int cx = chicken[i].first, cy = chicken[i].second;
+				unit_dist = min(unit_dist, abs(cx - x) + abs(cy - y));
+			}
+			sum += unit_dist;
+		}
+		ans = min(ans, sum);
+	} while (next_permutation(is_closed.begin(), is_closed.end()));
 	cout << ans;
 
 	return 0;
