484a6455fac6ae1864f4b285fffbf700ba4b01b8
splee <zlmmxe@naver.com>
2020-07-22 13:47:58 +0900

    Add BOJ3036

--- src/SolvingProblem/SolvingProblem.cpp
+++ src/SolvingProblem/SolvingProblem.cpp
@@ -2,29 +2,24 @@
 #define endl '\n'
 using namespace std;
 
-vector<bool> is_prime(10000001, true);
+int gcd(int a, int b) {
+	if (a == 0) return b;
+	return gcd(b % a, a);
+}
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	long long a, b;
-	cin >> a >> b;
-	is_prime[0] = false;
-	is_prime[1] = false;
-	for (long long i = 2; i * i * i * i <= b; i++) {
-		if (!is_prime[i]) continue;
-		for (long long j = i * i; j * j <= b; j += i) is_prime[j] = false;
+	int n;
+	cin >> n;
+	int x;
+	cin >> x;
+	while (--n) {
+		int y;
+		cin >> y;
+		int d = gcd(x, y);
+		cout << x / d << '/' << y / d << endl;
 	}
-	long long ans = 0;
-	for (long long i = 2; i * i <= b; i++) {
-		if (!is_prime[i]) continue;
-		long long j = i;
-		while (j <= b / i) {
-			j *= i;
-			if (j >= a) ans++;
-		}
-	}
-	cout << ans;
 
 	return 0;
 }
