b9420c01a49f1717a3d4a41d6bebe96a0d93bf39
splee <zlmmxe@naver.com>
2020-07-22 12:38:41 +0900

    Add BOJ1456

--- src/SolvingProblem/SolvingProblem.cpp
+++ src/SolvingProblem/SolvingProblem.cpp
@@ -2,14 +2,29 @@
 #define endl '\n'
 using namespace std;
 
+vector<bool> is_prime(10000001, true);
+
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int n;
-	cin >> n;
-	int i = 0;
-	for (; i * i <= n; i++);
-	cout << i - 1;
+	long long a, b;
+	cin >> a >> b;
+	is_prime[0] = false;
+	is_prime[1] = false;
+	for (long long i = 2; i * i * i * i <= b; i++) {
+		if (!is_prime[i]) continue;
+		for (long long j = i * i; j * j <= b; j += i) is_prime[j] = false;
+	}
+	long long ans = 0;
+	for (long long i = 2; i * i <= b; i++) {
+		if (!is_prime[i]) continue;
+		long long j = i;
+		while (j <= b / i) {
+			j *= i;
+			if (j >= a) ans++;
+		}
+	}
+	cout << ans;
 
 	return 0;
 }
