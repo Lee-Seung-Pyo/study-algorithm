fca5796ff41a6f0a100af240ff76599cb13127aa
SPLee123 <zlmmxe@naver.com>
2020-08-28 11:03:41 +0900

    Add BOJ1759

--- src/SolvingProblem/SolvingProblem.cpp
+++ src/SolvingProblem/SolvingProblem.cpp
@@ -2,79 +2,27 @@
 #define endl '\n'
 using namespace std;
 
-int board[1000][1000];
-bool vis[1000][1000];
-int dx[4] = { -1,1,0,0 };
-int dy[4] = { 0,0,-1,1 };
-queue<tuple<int, int, int>> q;
-
-void bfs() {
-	int w, h, x, y;
-	cin >> w >> h;
-	for (int i = 0; i < h; i++) fill(vis[i], vis[i] + w, false);
-	while (!q.empty()) q.pop();
-
-	for (int i = 0; i < h; i++) {
-		string s;
-		cin >> s;
-		for (int j = 0; j < w; j++) {
-			switch (s[j]) {
-			case '.':
-				board[i][j] = 0;
-				break;
-			case '#':
-				board[i][j] = -1;
-				break;
-			case '*':
-				board[i][j] = 1;
-				q.push({ i,j,1 });
-				break;
-			case '@':
-				board[i][j] = 0;
-				x = i;
-				y = j;
-				break;
-			}
-		}
-	}
-
-	while (!q.empty()) {
-		int cx, cy, cur;
-		tie(cx, cy, cur) = q.front(); q.pop();
-		for (int i = 0; i < 4; i++) {
-			int nx = cx + dx[i], ny = cy + dy[i];
-			if (nx < 0 || nx >= h || ny < 0 || ny >= w || board[nx][ny]) continue;
-			q.push({ nx,ny,cur + 1 });
-			board[nx][ny] = cur + 1;
-		}
-	}
-
-	q.push({ x,y,1 });
-	vis[x][y] = 1;
-	while (!q.empty()) {
-		int cx, cy, cur;
-		tie(cx, cy, cur) = q.front(); q.pop();
-		for (int i = 0; i < 4; i++) {
-			int nx = cx + dx[i], ny = cy + dy[i];
-			if (nx < 0 || nx >= h || ny < 0 || ny >= w) {
-				cout << cur << endl;
-				return;
-			}
-			if (vis[nx][ny] || board[nx][ny] && board[nx][ny] <= cur + 1) continue;
-			q.push({ nx,ny,cur + 1 });
-			vis[nx][ny] = 1;
-		}
-	}
-	cout << "IMPOSSIBLE" << endl;
-	return;
-}
-
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int tc;
-	cin >> tc;
-	while (tc--) bfs();
+	int l, c;
+	cin >> l >> c;
+	char C[15] = {};
+	for (int i = 0; i < c; i++) cin >> C[i];
+	sort(C, C + c);
+	vector<bool> select(l, 1);
+	select.insert(select.end(), c - l, 0);
 
+	string vowel = "aeiou";
+	do {
+		int vcnt = 0;
+		string result = "";
+		for (int i = 0; i < c; i++) {
+			if (!select[i]) continue;
+			if (vowel.find(C[i]) != -1) vcnt++;
+			result += C[i];
+		}
+		if (vcnt && vcnt < l - 1) cout << result << endl;
+	} while (prev_permutation(select.begin(), select.end()));
 	return 0;
 }
